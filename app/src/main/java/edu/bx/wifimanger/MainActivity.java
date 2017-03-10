edc0b645de4b8ed1cf1d0f10ca7b2c300a8ad21d
git <523928638@qq.com>
2017-03-10 17:19:08 +0800

    dasdds

--- app/src/main/java/edu/bx/wifimanger/MainActivity.java
+++ app/src/main/java/edu/bx/wifimanger/MainActivity.java
@@ -1,18 +1,27 @@
 package edu.bx.wifimanger;
 
-import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.ActionBar;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import edu.bx.wifimanger.adapter.WifiInfoAdapter;
 
@@ -29,38 +38,9 @@ public class MainActivity extends AppCompatActivity {
      * wifi管理器
      */
     private WifiManager wifiManger;
+    private View header;
+    private List<ScanResult> wifiMangerScanResults;
 
-    private Handler mOpenHandler=new Handler(){
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            if (checkWifiStatue()==WifiManager.WIFI_STATE_ENABLED){
-                activity_btn_wifi_switch.setVisibility(View.GONE);
-                activity_lv.setVisibility(View.VISIBLE);
-                mOpenHandler.removeMessages(0x100);
-
-            }else {
-                mOpenHandler.sendEmptyMessage(0x100);
-            }
-        }
-    };
-
-    private Handler mCloseHandler=new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-            if (checkWifiStatue()==WifiManager.WIFI_STATE_DISABLED){
-                activity_btn_wifi_switch.setVisibility(View.VISIBLE);
-                activity_lv.setVisibility(View.GONE);
-                mCloseHandler.removeMessages(0x110);
-
-            }else {
-                mCloseHandler.sendEmptyMessage(0x110);
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,8 +67,12 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         activity_btn_wifi_switch= (Button) findViewById(R.id.activity_wifi_switch);
         activity_lv= (ListView) findViewById(R.id.activity_rv);
-        initListLayout();
+       // initListLayout();
         initListener();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(new WifiReceiver(),filter);
     }
 
     /**
@@ -106,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
         if (checkWifiStatue()==WifiManager.WIFI_STATE_ENABLED){
             activity_btn_wifi_switch.setVisibility(View.GONE);
             activity_lv.setVisibility(View.VISIBLE);
+            activity_lv.setAdapter(null);
+            initListLayout();
 
 
         }else if (checkWifiStatue()==WifiManager.WIFI_STATE_DISABLED){
@@ -119,9 +105,54 @@ public class MainActivity extends AppCompatActivity {
      * 初始化wifi信息列表
      */
     private void initListLayout(){
-        activity_lv.setAdapter(new WifiInfoAdapter(this));
-        View header=View.inflate(this,R.layout.lv_header,null);
-        activity_lv.addHeaderView(header);
+        wifiMangerScanResults = wifiManger.getScanResults();
+        activity_lv.setAdapter(new WifiInfoAdapter(this, wifiMangerScanResults,wifiManger));
+        header = View.inflate(this, R.layout.lv_header,null);
+        if (activity_lv.getHeaderViewsCount()==0){
+            activity_lv.addHeaderView(header);
+        }
+        initHeader();
+
+        initLVItemClick();
+
+    }
+
+    /**
+     * 初始化wifi列表点击事件
+     */
+    private void initLVItemClick() {
+        activity_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv=new TextView(MainActivity.this);
+                        if (wifiMangerScanResults!=null){
+                            tv.setText(wifiMangerScanResults.get(position).SSID+"-------------"+position);
+                            BottomSheetDialog dialog=new BottomSheetDialog(MainActivity.this);
+                            dialog.setContentView(tv);
+                            dialog.show();
+                        }
+            }
+        });
+    }
+
+    /**
+     * 初始化头部数据（当前连接的wifi信息）
+     */
+    private void initHeader() {
+        WifiInfo connectionInfo = wifiManger.getConnectionInfo();
+        TextView title = (TextView) header.findViewById(R.id.header_tv_title);
+        TextView loc= (TextView) header.findViewById(R.id.header_tv_loc);
+        TextView xinhao= (TextView) header.findViewById(R.id.header_tv_xinhao);
+        String ssid = connectionInfo.getSSID();
+        title.setText(ssid.isEmpty()?"成功连接未知":"成功连接"+ssid);
+        //判断信号强度
+        if (Math.abs(connectionInfo.getRssi())<65){
+           xinhao.setText("信号：强");
+        }else if (65<=Math.abs(connectionInfo.getRssi())&&Math.abs(connectionInfo.getRssi())<75){
+            xinhao.setText("信号：中");
+        }else {
+            xinhao.setText("信号：弱");
+        }
     }
 
     /**
@@ -134,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
                 //当wifi关闭时才可以执行打开wifi操作
                 if (checkWifiStatue()==WifiManager.WIFI_STATE_DISABLED){
                     wifiManger.setWifiEnabled(true);
-                    mOpenHandler.sendEmptyMessage(0x100);
+
                 }
 
             }
@@ -155,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.wifi_switch:
                 if (checkWifiStatue()==WifiManager.WIFI_STATE_ENABLED){
                     wifiManger.setWifiEnabled(false);
-                    mCloseHandler.sendEmptyMessage(0x110);
+
                 }
                 break;
             case R.id.wifi_hot:
@@ -163,5 +194,43 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class WifiReceiver extends BroadcastReceiver{
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+//                mScanResults = mWifiManager.getScanResults();
+//                Log.d(TAG, "mScanResults.size()===" + mScanResults.size());
+                activity_btn_wifi_switch.setVisibility(View.GONE);
+                activity_lv.setAdapter(null);
+                activity_lv.setVisibility(View.VISIBLE);
+                initListLayout();
+
+            }
+//系统wifi的状态
+            else if (action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
+                int wifiState = intent.getIntExtra(
+                        WifiManager.EXTRA_WIFI_STATE, 0);
+                switch (wifiState) {
+                    case WifiManager.WIFI_STATE_ENABLED:
+//                        Log.d(TAG, "WiFi已启用" + DateUtils.getCurrentTime());
+                        wifiManger.startScan();
+                        break;
+                    case WifiManager.WIFI_STATE_DISABLED:
+                        activity_btn_wifi_switch.setVisibility(View.VISIBLE);
+                        activity_lv.setAdapter(null);
+                        activity_lv.removeHeaderView(header);
+                        activity_lv.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        }
+
+
     }
 }
--- app/src/main/java/edu/bx/wifimanger/adapter/WifiInfoAdapter.java
+++ app/src/main/java/edu/bx/wifimanger/adapter/WifiInfoAdapter.java
@@ -1,12 +1,17 @@
 package edu.bx.wifimanger.adapter;
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import edu.bx.wifimanger.R;
 
@@ -17,18 +22,24 @@ import edu.bx.wifimanger.R;
 public class WifiInfoAdapter extends BaseAdapter {
     private Context mCOntext;
 
-    public WifiInfoAdapter(Context mCOntext) {
+    private List<ScanResult> scanResults;
+
+    private WifiManager wifiManager;
+
+    public WifiInfoAdapter(Context mCOntext, List<ScanResult> scanResults,WifiManager wifiManager) {
         this.mCOntext = mCOntext;
+        this.scanResults=scanResults;
+        this.wifiManager=wifiManager;
     }
 
     @Override
     public int getCount() {
-        return 10;
+        return scanResults.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return scanResults.get(position);
     }
 
     @Override
@@ -38,6 +49,7 @@ public class WifiInfoAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ScanResult scanResult = scanResults.get(position);
         ViewHolder holder;
         if (convertView==null){
             convertView= LayoutInflater.from(mCOntext).inflate(R.layout.lv_item,null);
@@ -46,9 +58,25 @@ public class WifiInfoAdapter extends BaseAdapter {
         }else {
             holder= (ViewHolder) convertView.getTag();
         }
-        holder.xinhao.setImageResource(R.drawable.connect_signal_level_0);
-        holder.wifiName.setText("lxf");
-        holder.conn.setImageResource(R.drawable.aps_complain_checker);
+        //判断信号强度
+        if (Math.abs(scanResult.level)<60){
+            holder.xinhao.setImageResource(R.drawable.connect_signal_level_3);
+        }else if (60<=Math.abs(scanResult.level)&&Math.abs(scanResult.level)<70){
+            holder.xinhao.setImageResource(R.drawable.connect_signal_level_2);
+        }else if (70<=Math.abs(scanResult.level)&&Math.abs(scanResult.level)<80){
+            holder.xinhao.setImageResource(R.drawable.connect_signal_level_1);
+        }else {
+            holder.xinhao.setImageResource(R.drawable.connect_signal_level_0);
+        }
+        holder.wifiName.setText(scanResult.SSID);
+        if (wifiManager.getConnectionInfo().getSSID().replace(" ","").contains(scanResult.SSID)&& !scanResult.SSID
+                .isEmpty()){
+            holder.conn.setVisibility(View.VISIBLE);
+            holder.conn.setImageResource(R.drawable.aps_complain_checker);
+        }else {
+            holder.conn.setVisibility(View.GONE);
+        }
+
         return convertView;
     }
 
